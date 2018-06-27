05e684d2f49243244a2777c386be2bb275dda497
akansha_tayal <akansha_tayal@excellencetechnologies.in>
2018-06-27 13:24:26 +0530

    Modified code

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
 $(document).ready(function(){
-      $(".parent").click(function () {
-            $(".child").prop("checked", this.checked);
-      });
-      $(".child").click(function(){
-          if($(".child:checked").length>0) {
-              $(".parent").prop("checked", "checked");
-          } else {
-              $(".parent").prop("checked",this.checked);
-          }
-    });
+  $('input[type="checkbox"]').change(function() {
+
+         var checked = $(this).prop("checked"),
+             parentelem = $(this).parent();
+
+         parentelem.find('input[type="checkbox"]').prop({
+             checked: checked
+         });
+          checkSubchild(parentelem);
+         function checkSubchild(check) {
+             var parent = check.parent().parent(),
+                 flag = false;
+
+             check.siblings().each(function() {
+                 return flag = ($(this).children('input[type="checkbox"]').prop("checked") === checked);
+             });
+             if (flag && checked) {
+                 parent.children('input[type="checkbox"]').prop("checked", checked);
+             } else if (flag && !checked) {
+                 parent.children('input[type="checkbox"]').prop("checked", checked);
+             } else {
+                 check.parents("li").children('input[type="checkbox"]').prop({
+                     checked: true
+                 });
+             }
+         }
+     });
 });
